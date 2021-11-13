58060afc3637418cca889866f950d1b68857082f
nir6760 <58267005+nir6760@users.noreply.github.com>
2021-11-13 10:33:30 +0200

    fixed paths

--- HTTPHandler.py
+++ HTTPHandler.py
@@ -1,7 +1,8 @@
 import os
 import hw1_utils
 from pdfminer import high_level
-from glob import glob
+import pathlib
+from urllib.parse import urlparse
 
 # get all pdf files recursively
 def get_all_pdf_files():
@@ -11,14 +12,14 @@ def get_all_pdf_files():
             all_files.append(os.path.join(dirpath, filename))
     return all_files
 
-#find file path by name
+
+# find file path by name
 def find_file_path_by_name(file_name_in):
     for dirpath, dirnames, filenames in os.walk("pdfs"):
         for filename in [f for f in filenames if f.endswith(".pdf")]:
             if file_name_in == filename:
-             return os.path.join(dirpath, filename)
+                return os.path.join(dirpath, filename)
     return None
-
 
 
 # create png html file as string
@@ -28,7 +29,7 @@ def create_wordcloud_page(title, png_path):
                 <title>png_page</title>\n\
             </head>\n\
             <body>\n\
-            <h1>" + title[1:] + " </h1>\n\
+            <h1>" + title + " </h1>\n\
             <img src=\"/" + png_path + "\">\n\
             </br></br>\n\
             <button onclick=\"home_red()\">Go back</button>\n\
@@ -59,8 +60,11 @@ def create_home_page(all_pdf_files):
                 <p>choose a .pdf file to generate a wordcloud from:</p>\n\
                 <ul style=\"list-style-type:disc\">\n"
     for pdf_file_path in all_pdf_files:
-        #pdf_name = os.path.basename(pdf_file_path)
-        pdf_name = pdf_file_path[5:]
+        # pdf_name = os.path.basename(pdf_file_path)
+        pdf_name = pathlib.Path(pdf_file_path)
+        pdf_name = pathlib.Path(*pdf_name.parts[1:])  # remove pdfs// from path
+        pdf_name = str(pdf_name)
+        # pdf_name = pdf_file_path[5:]
         pdf_name_without_ext = "\'" + os.path.splitext(pdf_name)[0] + "\'"
         pdf_name_without_ext = pdf_name_without_ext.replace('\\', '/')
         content_li = "<li><button onclick=\"myFunction(" + pdf_name_without_ext + ")\">" + pdf_name + "</button></li>\n"
@@ -78,34 +82,37 @@ def create_home_page(all_pdf_files):
 
 # read pdf file from pdf path and create wordcloud png
 def pdf_to_wordcloud(pdf_path, stop_words_list):
-    pdf_name = os.path.basename(pdf_path)
+    pdf_name = os.path.split(pdf_path)[1]
     pdf_string = high_level.extract_text(pdf_path)
     pdf_string_filtered = [word for word in pdf_string.split() if not word in stop_words_list]
-    png_path = 'html_pages/png_images/' + pdf_name.split('.')[0] + '.png'
     pdf_string_filtered = " ".join(pdf_string_filtered)
+    saving_folder_png = os.path.join('html_pages', 'png_images')
+    png_path = os.path.join(saving_folder_png, os.path.splitext(pdf_name)[0] + '.png')
     hw1_utils.generate_wordcloud_to_file(pdf_string_filtered, png_path)
     return png_path
 
 
 class HTTPHandler:
-    def get(self, filename, type):
+    def get(self, filename, file_type):
         with open('stopwords.txt') as text_file:
             stop_words_list = text_file.read().split()
-        if not os.path.isdir('pdfs'): # pdfs folder doesnt exist
-            print('pdfs folder dosn\'t exist')
-            raise FileNotFoundError
+        # if not os.path.isdir('pdfs'): # pdfs folder doesnt exist
+        #    print('pdfs folder dosn\'t exist')
+        #    raise FileNotFoundError
         all_pdf_files = get_all_pdf_files()
-        if type.find("image") == -1:
+        if file_type.find("image") == -1:
             if filename == '/':
                 content = create_home_page(all_pdf_files)
             else:
                 filename += ".pdf"
-                file_path = "pdfs\\" + filename[1:].replace("/", "\\")
-                #file_path = find_file_path_by_name(file_path)
+                components = filename[1:].split('/')
+                file_path_rel_pdfs = os.path.join(*components)
+                file_path = os.path.join('pdfs', *components)
+                # file_path = find_file_path_by_name(file_path)
                 if file_path not in all_pdf_files:
                     raise FileNotFoundError
                 png_path = pdf_to_wordcloud(file_path, stop_words_list)  # creates the png
-                content = create_wordcloud_page(filename, png_path)
+                content = create_wordcloud_page(file_path_rel_pdfs, png_path)
 
         else:  # this is image
             image_data = open(filename[1:], 'rb')  # file name is png path
--- hw1.py
+++ hw1.py
@@ -5,17 +5,18 @@ import hw1_utils
 import HTTPHandler
 import time
 
-
 """
- Implements a simple HTTP/1.0 Server
+ Implements a simple HTTP/1.1 Server
 
 """
 # Define socket host and port
 SERVER_HOST = '127.0.0.1'
 SERVER_PORT = 8888
+SERVER_SOCKET_NUM = 8888
 NUM_BYTES_REQUEST = 4096
 
-#create error html string
+
+# create error html string
 def create_error_html(error_num):
     error_html = "<html>\n\
                 <head>\n\
@@ -34,18 +35,18 @@ def create_error_html(error_num):
                 </html>"
     return error_html
 
-def make_http_time_string(time_struct):
 
+def make_http_time_string(time_struct):
     '''Input struct_time and output HTTP header-type time string'''
 
     return time.strftime('%a, %d %b %Y %H:%M:%S GMT',
 
-            time_struct)
+                         time_struct)
+
 
 # handle request method
 def handle_request(request):
-
-    http = HTTPHandler.HTTPHandler
+    http = HTTPHandler.HTTPHandler()
     # Parse headers
     print(request)
     # Parse HTTP headers
@@ -61,13 +62,14 @@ def handle_request(request):
         filename = req_list[1]
 
         if req_list[0] == "GET":
-            content = http.get(None, filename, type_content[0])
+            content = http.get(filename, type_content[0])
             return 200, content
         else:
             content = create_error_html(501)
             return 501, content
     except FileNotFoundError:
-        with open('html_pages/not_found_page.html') as not_found_file:
+        not_found_page = os.path.join('html_pages', 'not_found_page.html')
+        with open(not_found_page) as not_found_file:
             not_found_content = not_found_file.read()
         return 404, not_found_content
 
@@ -75,16 +77,17 @@ def handle_request(request):
 if __name__ == "__main__":
     while True:
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
-            server_socket.bind(('localhost', 8888))
+            server_socket.bind(('localhost', SERVER_PORT))
             server_socket.settimeout(None)
-            server_socket.listen(1)
+            server_socket.listen(SERVER_SOCKET_NUM)
             print('Listening on port %s ...' % SERVER_PORT)
             # Wait for client connections
             client_connection, addr = server_socket.accept()
             with client_connection:
                 # Get the client request
                 request = client_connection.recv(NUM_BYTES_REQUEST)
-                if not request or request== "\r\n".encode():
+                if not request or request == "\r\n".encode():
+                    client_connection.close()
                     continue
                 try:
                     status, content = handle_request(request)
@@ -95,7 +98,7 @@ if __name__ == "__main__":
                                 # Send HTTP response
                                 client_connection.sendall('HTTP/1.1 200 OK\r\n'.encode())
                                 client_connection.sendall(("Date: " + make_http_time_string(time.localtime())
-                                                          + "\r\n").encode())
+                                                           + "\r\n").encode())
                                 client_connection.sendall("Content-Type: text/html\r\n\r\n".encode())
                                 client_connection.sendall(content.encode())
                             else:  # image
@@ -105,28 +108,29 @@ if __name__ == "__main__":
                                                            + "\r\n").encode())
                                 client_connection.sendall("Content-Type: image/png\r\n".encode())
                                 client_connection.sendall("Accept-Ranges: bytes\r\n\r\n".encode())
-                                client_connection.sendall(content) #allready in bytes
+                                client_connection.sendall(content)  # allready in bytes
                         else:
-                            client_connection.sendall(('HTTP/1.1 500'+' Internal Server Error\r\n').encode())
+                            client_connection.sendall(('HTTP/1.1 500' + ' Internal Server Error\r\n').encode())
                             client_connection.sendall(("Date: " + make_http_time_string(time.localtime())
                                                        + "\r\n").encode())
                             client_connection.sendall("Content-Type: text/html\r\n\r\n".encode())
                             client_connection.sendall(create_error_html(500).encode())
                     else:  # 404 or 501
-                        err_str = ' NOT FOUND\r\n' if status==404 else ' NOT GET\r\n'
+                        err_str = ' NOT FOUND\r\n' if status == 404 else ' NOT GET\r\n'
                         client_connection.sendall(
                             ('HTTP/1.1 ' + str(status) + err_str).encode())
                         client_connection.sendall(("Date: " + make_http_time_string(time.localtime())
                                                    + "\r\n").encode())
                         client_connection.sendall("Content-Type: text/html\r\n\r\n".encode())
                         client_connection.sendall(content.encode())
-                except Exception as e:# 500
+                except Exception as e:  # 500
                     print(e)
                     client_connection.sendall(('HTTP/1.1 500' + ' Internal Server Error\r\n').encode())
                     client_connection.sendall(("Date: " + make_http_time_string(time.localtime())
                                                + "\r\n").encode())
                     client_connection.sendall("Content-Type: text/html\r\n\r\n".encode())
                     client_connection.sendall(create_error_html(500).encode())
-                client_connection.close()
+                finally:
+                    client_connection.close()
     # Close socket
-    #server_socket.close()
+    # server_socket.close()
